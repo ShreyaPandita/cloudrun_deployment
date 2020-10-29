ab1beb0a32951ce6b7f40632b2d9d263f8ba51f2
ShreyaPandita <shreya.pandita@gmail.com>
2020-10-29 14:48:21 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello Awesome with cloud run')
+    res.end('Hello Awesome with cloud run v2')
 })
 const PORT = process.env.PORT || 8080
 server.listen(PORT, () => console.log('Listening'))
